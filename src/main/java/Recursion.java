21ab40adba948c55a2736543e80e2d5391bf262b
Jason Homan <jason.homan@willowtreeapps.com>
2022-11-23 16:12:36 -0500

    finished easy recursion section

---
+++ src/main/java/Recursion.java
@@ -0,0 +1,89 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Recursion {
+
+    public static int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+
+        return base * power(base, exponent - 1);
+    }
+    /*
+    base case: if exponent = 0 return 1
+
+    power(2, 4): 2 * power(2, 3)
+        power(2, 3): 2 * power(2, 2)
+            power(2, 2): 2 * power(2, 1)
+                power(2, 1): 2 * power(2, 0)
+     */
+
+    public static int factorial(int number) {
+        if (number == 1) {
+            return 1;
+        }
+
+        return number * factorial(number - 1);
+    }
+    /*
+    base case: factorial(1) -> 1
+
+    factorial(5): 5 * factorial(4)
+        factorial(4): 4 * factorial(3)
+            factorial(3): 3 * factorial(2)
+                factorial(2): 2 * factorial(1)
+                    factorial(1): 1
+     */
+
+    public static int productOfArray(int[] intArray) {
+        if (intArray.length == 1) {
+            return intArray[0];
+        }
+
+        return intArray[0] * productOfArray(Arrays.copyOfRange(intArray, 1, intArray.length));
+    }
+    /*
+    base case: productOfArray(n) -> n
+
+    productOfArray(4,3,2): 2 * productOfArray(4, 3)
+        productOfArray(4,3): 3 * productOfArray(4)
+            productOfArray(4): 4
+     */
+
+    public static int recursiveRange(int number) {
+        if (number == 1) {
+            return 1;
+        }
+
+        return number + recursiveRange(number - 1);
+    }
+    /*
+    base case: recursiveRange(1) -> 1
+
+    recursiveRange(4): 4 + recursiveRange(3)
+        recursiveRange(3): 3 + recursiveRange(2)
+            recursiveRange(2): 2 + recursiveRange(1)
+                recursiveRange(1): 1
+     */
+
+    public static int fib(int number) {
+        if (number == 1 || number == 2) {
+            return 1;
+        }
+
+        return fib(number - 1) + fib(number - 2);
+    }
+    /*
+    base case: fib(1) and fib(2) = 1
+
+    fib(7): fib(5) + fib(6): 13
+        fib(6): fib(4) + fib(5): 8
+            fib(5): fib(3) + fib(4): 5
+                fib(4): fib(2) + fib(3): 3
+                    fib(3): fib(1) + fib(2): 2
+                        fib(2): 1: 1
+                            fib(1): 1: 1
+     */
+}
+
---
+++ src/test/java/RecursionTest.java
@@ -0,0 +1,105 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class RecursionTest {
+
+    @Test
+    @DisplayName("Should return the resulting power")
+    public void powerTest() {
+        // when
+        int result1 = Recursion.power(2, 0);
+        int result2 = Recursion.power(2, 2);
+        int result3 = Recursion.power(2, 4);
+        int result4 = Recursion.power(4, 4);
+//        int result5 = Recursion.power(2, -4);
+
+        // then
+        assertAll("",
+                () -> assertThat(result1).isEqualTo(1),
+                () -> assertThat(result2).isEqualTo(4),
+                () -> assertThat(result3).isEqualTo(16),
+                () -> assertThat(result4).isEqualTo(256)
+        );
+    }
+
+    @Test
+    @DisplayName("Should return the product of an integer and all the integers below it")
+    public void factorialTest() {
+        // when
+        int result1 = Recursion.factorial(1);
+        int result2 = Recursion.factorial(2);
+        int result3 = Recursion.factorial(4);
+        int result4 = Recursion.factorial(7);
+//        int result5 = Recursion.factorial(-4);
+
+        // then
+        assertAll("",
+                () -> assertThat(result1).isEqualTo(1),
+                () -> assertThat(result2).isEqualTo(2),
+                () -> assertThat(result3).isEqualTo(24),
+                () -> assertThat(result4).isEqualTo(5040)
+        );
+    }
+
+    @Test
+    @DisplayName("Should return a product of all numbers in array")
+    public void productOfArrayTest() {
+        // given
+        int[] array1 = {1,2,3};
+        int[] array2 = {1,2,3,10};
+        int[] array3 = {1,0,3};
+        int[] array4 = {1,-2,3};
+
+        // when
+        int result1 = Recursion.productOfArray(array1);
+        int result2 = Recursion.productOfArray(array2);
+        int result3 = Recursion.productOfArray(array3);
+        int result4 = Recursion.productOfArray(array4);
+
+        // then
+        assertAll("",
+                () -> assertThat(result1).isEqualTo(6),
+                () -> assertThat(result2).isEqualTo(60),
+                () -> assertThat(result3).isEqualTo(0),
+                () -> assertThat(result4).isEqualTo(-6)
+        );
+    }
+
+    @Test
+    @DisplayName("Should return a the sum of number passed in and all numbers below it, down until 0")
+    public void recursiveRangeTest() {
+        // when
+        int result1 = Recursion.recursiveRange(6);
+        int result2 = Recursion.recursiveRange(10);
+//        int result4 = Recursion.recursiveRange(-2);
+
+        // then
+        assertAll("",
+                () -> assertThat(result1).isEqualTo(21),
+                () -> assertThat(result2).isEqualTo(55)
+//                () -> assertThat(result3).isEqualTo(0)
+//                () -> assertThat(result4).isEqualTo(-6)
+        );
+    }
+
+    @Test
+    @DisplayName("Should return the nth number in the Fibonacci sequence")
+    public void fibTest() {
+        // when
+        int result1 = Recursion.fib(4);
+        int result2 = Recursion.fib(10);
+        int result3 = Recursion.fib(28);
+        int result4 = Recursion.fib(35);
+
+        // then
+        assertAll("",
+                () -> assertThat(result1).isEqualTo(3),
+                () -> assertThat(result2).isEqualTo(55),
+                () -> assertThat(result3).isEqualTo(317811),
+                () -> assertThat(result4).isEqualTo(9227465)
+        );
+    }
+}
